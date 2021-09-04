5bff3c44586088d973c87a862abddeabb75a6266
ricky9397 <kimzzan1234@gmail.com>
2021-09-05 05:34:12 +0900

    commit

--- projectTest.sql
+++ projectTest.sql
@@ -6,6 +6,10 @@ insert into project.chatlist values(2);
 
 select * from chatroom;
 select * from chatlist;
+select * from carry;
+select * from member;
+select * from chatroom;
+select * from chatlist;
 
 delete from project.chatroom where messageidx=3;
 
@@ -81,22 +85,13 @@ CREATE TABLE `CARRY` (
   `CRID` varchar(30) NOT NULL COMMENT '캐리아이디',
   `CRPW` varchar(30) NOT NULL COMMENT '비밀번호',
   `CRNAME` varchar(50) NOT NULL COMMENT '캐리실명',
-  `CRNICK` varchar(50) NOT NULL COMMENT '캐리닉네임',
-  `CRGENDER` varchar(5) NOT NULL COMMENT '성별',
-  `CRPHONE` varchar(50) NOT NULL COMMENT '캐리연락처',
+  `CRNICK` varchar(50) COMMENT '캐리닉네임',
+  `CRGENDER` varchar(5) COMMENT '성별',
+  `CRPHONE` varchar(50) COMMENT '캐리연락처',
   `CRINTRO` mediumtext COMMENT '소갯말',
   `CRDEPART` mediumtext COMMENT '전문분야',
   `CRFIELD` mediumtext COMMENT '종목',
-  `CRCERTI1` mediumtext COMMENT '자격및경력1',
-  `CRCERTI2` mediumtext COMMENT '자격및경력2',
-  `CRCERTI3` mediumtext COMMENT '자격및경력3',
-  `CRCERTI4` mediumtext COMMENT '자격및경력4',
-  `CRCERTI5` mediumtext COMMENT '자격및경력5',
-  `PROPRICE1` int NOT NULL COMMENT '수업1회가격',
-  `PROPRICE5` int NOT NULL COMMENT '수업5회가격',
-  `PROPRICE10` int NOT NULL COMMENT '수업10회가격',
-  `PROPRICE20` int NOT NULL COMMENT '수업20회가격',
-  `FACEORNOT` tinyint(1) NOT NULL COMMENT '비대면&대면',
+  `FACEORNOT` tinyint(1) NULL COMMENT '비대면&대면',
   `PLACENAME` varchar(255) DEFAULT NULL COMMENT '소속플레이스',
   `PLACEIDX` int DEFAULT NULL COMMENT '플레이스IDX',
   PRIMARY KEY (`CRIDX`),
@@ -104,6 +99,9 @@ CREATE TABLE `CARRY` (
   CONSTRAINT `FK_PLACE_TO_CARRY` FOREIGN KEY (`PLACEIDX`) REFERENCES `PLACE` (`PLACEIDX`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='캐리정보';
 
+select * from carry;
+
+insert into carry (crid, crpw, crname, crnick, placename) values ('test3', 1234, '핏블리', '핏블리', '핏블리짐');
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -153,12 +151,13 @@ CREATE TABLE `CHATROOM` (
   `MESSAGEIDX` int NOT NULL AUTO_INCREMENT COMMENT '메세지 번호',
   `CHATIDX` int NOT NULL COMMENT '채팅방번호',
   `CHATCONTENT` mediumtext NOT NULL COMMENT '대화내용',
-  `CHATDATE` timestamp NOT NULL COMMENT '대화시간',
+  `CHATDATE` timestamp default current_timestamp COMMENT '대화시간',
   `CRIDX` int NOT NULL COMMENT '캐리번호',
   `MEMIDX` int NOT NULL COMMENT '회원번호',
   `CONTENTTYPE` INT DEFAULT 0 COMMENT '0=유저대화, 1=캐리대화',
   `CHATPOSITION` INT DEFAULT 0 COMMENT '0=방에서 안나감, 1=방에서나감',
   `CHATREAD` INT DEFAULT 0 COMMENT '0=읽지않음, 1=읽음',
+  `CHATLIKE` INT DEFAULT 0 COMMENT '0=좋아여, 1=좋아요',
   PRIMARY KEY (`MESSAGEIDX`),
   KEY `FK_CARRY_TO_CHATROOM` (`CRIDX`),
   KEY `FK_MEMBER_TO_CHATROOM` (`MEMIDX`),
@@ -170,5 +169,10 @@ CREATE TABLE `CHATROOM` (
 insert into chatroom (chatidx, chatcontent, cridx, memidx) values(1, '안녕하세요', 1, 1);
 
 select * from chatroom where chatidx=1;
+select * from chatroom;
+select * from chatlist where cridx=2 and memidx=1;
+select count(*) from chatlist where chatidx=2;
+select * from chatlist;
 
+select chatcontent, chatdate from chatroom where chatidx=1 order by chatdate desc;
 
